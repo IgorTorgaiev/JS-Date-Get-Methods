13042621cf64ac1d9c03047d52175deb42e07ecc
Igor Torgaiev <Itorgaiev@gmail.com>
2021-01-03 20:05:52 +0100

    JS. Date: Get methods

---
+++ script.js
@@ -0,0 +1,45 @@
+// getFullYear
+
+var a = new Date();
+document.getElementById("demo1").innerHTML = a.getFullYear();
+
+// getMonth
+
+var b = new Date();
+document.getElementById("demo2").innerHTML = b.getMonth() + 1;
+
+// getDate
+
+var c = new Date();
+document.getElementById("demo3").innerHTML = c.getDate();
+
+// getHours
+
+var d = new Date();
+document.getElementById("demo4").innerHTML = d.getHours();
+
+// getMinutes
+
+var e = new Date();
+document.getElementById("demo5").innerHTML = e.getMinutes();
+
+// getSeconds
+
+var f = new Date();
+document.getElementById("demo6").innerHTML = f.getSeconds();
+
+// getMilliseconds
+
+var g = new Date();
+document.getElementById("demo7").innerHTML = g.getMilliseconds();
+
+// getTime
+
+var h = new Date();
+document.getElementById("demo8").innerHTML = h.getTime();
+
+
+// getDay
+
+var i = new Date();
+document.getElementById("demo9").innerHTML = i.getDay();
